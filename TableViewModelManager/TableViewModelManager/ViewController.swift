251a8c83f0571314b18990d8b396069432967708
George Green <george@theswift.guru>
2015-11-08 14:38:37 +0000

    Created the Xcode project.

---
+++ TableViewModelManager/TableViewModelManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewModelManager
+//
+//  Created by George Green on 08/11/2015.
+//  Copyright © 2015 George Green of London. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
